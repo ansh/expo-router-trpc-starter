e31b05aa7049f164ed0f88aa6835b84fe209c669
Ansh Nanda <anshnanda10@gmail.com>
2024-09-18 15:33:31 +0530

    Fix tRPC setup for browser

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,15 +1,8 @@
 import { trpc } from "@/constants/trpc";
-import { StyleSheet, Text } from "react-native";
+import { ThemedText } from "./ThemedText";
 
 export function Hello() {
   const [hello] = trpc.sample.hello.useSuspenseQuery({ text: "World" });
 
-  return <Text style={styles.title}>{hello.greeting}</Text>;
+  return <ThemedText type="title">{hello.greeting}</ThemedText>;
 }
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
--- constants/trpc.tsx
+++ constants/trpc.tsx
@@ -11,12 +11,15 @@ import SuperJSON from "superjson";
 
 // Figure out the base URL of the API
 const getBaseUrl = (): string => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (typeof window !== "undefined") return ""; // browser should use relative URL
   if (__DEV__) {
     const isEmulator = !Device.isDevice;
+    const isWeb = Platform.OS === "web";
     const localUrl =
-      Platform.OS === "ios" ? "http://localhost:8081" : "http://10.0.2.2:8081";
-    return isEmulator ? localUrl : "http://192.168.68.111:8081"; // Change to your local IP for real device
+      Platform.OS === "android"
+        ? "http://10.0.2.2:8081"
+        : "http://localhost:8081";
+    return isEmulator || isWeb ? localUrl : "http://192.168.68.111:8081"; // Change to your local IP for real device
   }
   return "https://your-production-url.com"; // TODO: Add your production URL here
 };
